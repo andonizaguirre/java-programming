8ec08733ed1ddd9abb767cbb7ad4b64fd940a3eb
andonizaguirre <48142479+andonizaguirre@users.noreply.github.com>
2023-05-18 20:41:08 -0500

    Part 11, 1-10

---
+++ mooc-java-programming-ii/part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
@@ -0,0 +1,13 @@
+package FlightControl;
+
+import FlightControl.logic.FlightControl;
+import FlightControl.ui.TextUI;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write the main program here. It is useful to create some classes of your own.
+        new TextUI(new FlightControl(), new Scanner(System.in)).start();                
+    }
+}
